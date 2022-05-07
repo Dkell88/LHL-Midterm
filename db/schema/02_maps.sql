154dcba67590f9b1ae94d8656e40428a60642b1d
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-06 23:56:27 -0500

    Add maps table schema

---
+++ db/schema/02_maps.sql
@@ -0,0 +1,12 @@
+-- Drop and recreate maps table
+
+DROP TABLE IF EXISTS maps CASCADE;
+CREATE TABLE maps (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  is_active BOOLEAN DEFAULT TRUE
+);
