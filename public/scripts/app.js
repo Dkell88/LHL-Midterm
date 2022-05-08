258d831db17281ed5612ea9af48543c430872436
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-08 15:16:17 -0500

    Add google search place api

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,27 +1,58 @@
 // Client facing scripts here
 $(() => {
-  renderMap();
+  getCurrentUserLocation();
 });
 
-const renderMap = () => {
+const getCurrentUserLocation = () => {
   navigator.geolocation.getCurrentPosition((position) => {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-
-    const map = L.map("map").setView([latitude, longitude], 13);
-
-    L.tileLayer(
-      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-      {
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: "mapbox/streets-v11",
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken:
-          "pk.eyJ1IjoiZ2FnYW5zaW5naDMiLCJhIjoiY2wyd2RqY3l2MDg2NjNjcXU5cDNoaHJmbyJ9.WaGj9j3kU-EKhz0-tQSD6w",
-      }
-    ).addTo(map);
+    return renderMap(latitude, longitude);
   });
 };
+
+const renderMap = (lat, long) => {
+  const map = L.map("map").setView([lat, long], 13);
+  const mapTiles = {
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken:
+      "pk.eyJ1IjoiZ2FnYW5zaW5naDMiLCJhIjoiY2wyd2RqY3l2MDg2NjNjcXU5cDNoaHJmbyJ9.WaGj9j3kU-EKhz0-tQSD6w",
+  };
+
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    mapTiles
+  ).addTo(map);
+
+  addGoogleSearch(map);
+};
+
+const addGoogleSearch = (myMap) => {
+  const GooglePlacesSearchBox = L.Control.extend({
+    onAdd: function () {
+      const element = document.createElement("input");
+      element.id = "searchBox";
+      return element;
+    },
+  });
+  new GooglePlacesSearchBox().addTo(myMap);
+
+  const input = document.getElementById("searchBox");
+  const searchBox = new google.maps.places.SearchBox(input);
+
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
+
+    //we can also use .panTo
+    myMap.flyTo(
+      [places[0].geometry.location.lat(), places[0].geometry.location.lng()],
+      8
+    );
+  });
+};
+
+// if (places.length == 0) {
+//   return;
+// }
