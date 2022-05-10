726354fadeb692306081b653d50d9264117c7beb
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-09 23:34:08 -0500

    Add maps router

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,109 +1,125 @@
 // Client facing scripts here
 let POINT_ID = 0;
 $(() => {
+  const map = L.map("map", {
+    doubleClickZoom: false,
+  });
 
-  const loadMap = function() {
-    const map = L.map('map', {
-      doubleClickZoom: false}
-      ).setView([49.262838, -122.781071], 16);
-  
-      return map;
-  };
-    
-    const renderMap = function(map) {
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  getCurrentUserLocation(map);
+
+  const mapId = 1;
+
+  $("#show-map").click(() => {
+    $.ajax(`/maps/${mapId}`, { method: "GET" }).then((data) => {
+      map.panTo(new L.LatLng(data.latitude, data.longitude));
+      console.log(data);
+    });
+
+    $.ajax(`/points/${mapId}`, { method: "GET" }).then((data) => {
+      console.log(data);
+      const testMarker = L.marker([data.latitude, data.longitude]).addTo(map);
+    });
+  });
+});
+
+const getCurrentUserLocation = (map) => {
+  navigator.geolocation.getCurrentPosition((position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    map.setView([latitude, longitude], 16);
+
+    return loadMap(map);
+  });
+};
+
+const loadMap = function (map) {
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
-      id: 'mapbox/streets-v11',
+      id: "mapbox/streets-v11",
       tileSize: 512,
       zoomOffset: -1,
-      accessToken: 'pk.eyJ1IjoiZGtlbGw4OCIsImEiOiJjbDJ3Zm44NjMwZjVqM2RxY3gyN3J6dXJ2In0.SYE3QdtfFxH63YvUTI7FMA'
-    }).addTo(map);
-  
-    const marker2 = L.marker([49.277, -122.78]).addTo(map);
-   
-    L.control.scale({
+      accessToken:
+        "pk.eyJ1IjoiZGtlbGw4OCIsImEiOiJjbDJ3Zm44NjMwZjVqM2RxY3gyN3J6dXJ2In0.SYE3QdtfFxH63YvUTI7FMA",
+    }
+  ).addTo(map);
+
+  const marker2 = L.marker([49.277, -122.78]).addTo(map);
+
+  L.control
+    .scale({
       metric: true,
       imperial: false,
-      position: 'bottomright'
-    }).addTo(map);
-  };
-  
-  const map = loadMap();
-  renderMap(map);
+      position: "bottomright",
+    })
+    .addTo(map);
 
-  function onMapClick(event) {
-   
-    if($('#pin-deets').is(":visible")){
-      console.log("yes pin deeets is visible")
-    } 
-    if(!$('#pin-deets').is(":visible")){
-      console.log("Nope pin deeets isn't visible")
-      $('#pin-deets').show();
-     
-      const point = {
-        mapId: 41,
-        title: "",
-        description: "",
-        imageURL: "",
-        latitude: event.latlng.lat,
-        longitude: event.latlng.lng
-      }
-      // console.log(event.latlng);
-      // console.log(point);
+  map.on("click", onMapClick);
+};
 
-      //addPoint(point); 
+function onMapClick(event) {
+  if ($("#pin-deets").is(":visible")) {
+    console.log("yes pin deeets is visible");
+  }
+  if (!$("#pin-deets").is(":visible")) {
+    console.log("Nope pin deeets isn't visible");
+    $("#pin-deets").show();
 
-       $.post('points/', point)
-       .then(pointAdded => {
-         console.log("point added to DB ", pointAdded)
-         console.log("The pointAdded.id is: ",  pointAdded.id)
-         POINT_ID = pointAdded.id;
-         console.log("POINT_ID is now: ", POINT_ID)
-       })
-      //$.get('/points/');
-    }
-    
-  };
-  
-  $('#pin-deets').submit(function(event) {
-    console.log("Sumbitted");
-    event.preventDefault();
-    const kids = $(this).children();
-    const pointToEdit = {
-      title: $(kids[0]).val(),
-      description: $(kids[1]).val(),
-      imageURL: $(kids[2]).val()
+    const point = {
+      mapId: 41,
+      title: "",
+      description: "",
+      imageURL: "",
+      latitude: event.latlng.lat,
+      longitude: event.latlng.lng,
     };
+    // console.log(event.latlng);
+    // console.log(point);
 
+    //addPoint(point);
 
-    if(!pointToEdit.title || !pointToEdit.description || !pointToEdit.imageURL) {
-      return console.log("error missing a title, description, or image URL");
-    }
-    
-    let markerPopup = `
+    $.post("points/", point).then((pointAdded) => {
+      console.log("point added to DB ", pointAdded);
+      console.log("The pointAdded.id is: ", pointAdded.id);
+      POINT_ID = pointAdded.id;
+      console.log("POINT_ID is now: ", POINT_ID);
+    });
+    //$.get('/points/');
+  }
+}
+
+$("#pin-deets").submit(function (event) {
+  console.log("Sumbitted");
+  event.preventDefault();
+  const kids = $(this).children();
+  const pointToEdit = {
+    title: $(kids[0]).val(),
+    description: $(kids[1]).val(),
+    imageURL: $(kids[2]).val(),
+  };
+
+  if (!pointToEdit.title || !pointToEdit.description || !pointToEdit.imageURL) {
+    return console.log("error missing a title, description, or image URL");
+  }
+
+  let markerPopup = `
       <section class = "pin-popus">
       <span>${pointToEdit.title}</span><br>
       <span>${pointToEdit.description}</span><br>
       <img src="${pointToEdit.imageURL}">
-      </section>`
-    //console.log(markerPopup);
+      </section>`;
+  //console.log(markerPopup);
 
-
-    //$.get(`/users/${USER_ID}`)
-    //$.get(`/points/${POINT_ID}`, pointToEdit)
-    $.post(`/points/${POINT_ID}/edit`, pointToEdit)
-    .then(point => {
-      console.log("point returned after GET /points/:id: ", point)
-      let marker = new L.marker([point.latitude, point.longitude]);
-      marker.bindPopup(markerPopup).openPopup();
-      map.addLayer(marker);
-      $('#pin-deets').hide()
-    })
-
-  
+  //$.get(`/users/${USER_ID}`)
+  //$.get(`/points/${POINT_ID}`, pointToEdit)
+  $.post(`/points/${POINT_ID}/edit`, pointToEdit).then((point) => {
+    console.log("point returned after GET /points/:id: ", point);
+    let marker = new L.marker([point.latitude, point.longitude]);
+    marker.bindPopup(markerPopup).openPopup();
+    map.addLayer(marker);
+    $("#pin-deets").hide();
   });
-
-  map.on('click', onMapClick);
-
 });
--- routes/points.js
+++ routes/points.js
@@ -5,94 +5,104 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 // Once teh queries are working we'll pull them out to a seperate file
 //const userQueries = require('../db/points-queries')
 
 const pointRouter = (db) => {
-
   //GET /points/
   router.get("/", (req, res) => {
     let query = `SELECT * FROM points`;
     console.log(query);
     db.query(query)
-      .then(data => {
+      .then((data) => {
         const points = data.rows;
         res.json({ points });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
-    });
+  });
 
-    router.get("/:id", (req, res) => {
-      let query = `SELECT * FROM points WHERE id = $1`;
-      let queryParams = [req.params.id]
-      console.log("The query parameters for the GET /points/:id is: ",queryParams);
-      db.query(query, queryParams)
-        .then(point => {
-          console.log("Point returned by /:id query is: ", point.rows[0])
-          res.send(point.rows[0])
-        })
-        .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message });
-        });
+  router.get("/:mapId", (req, res) => {
+    // let query = `SELECT * FROM points WHERE id = $1`;
+    let query = `
+    SELECT points.latitude, points.longitude
+    FROM points
+    JOIN maps ON maps.id = map_id
+    WHERE maps.id = $1`;
+
+    let queryParams = req.params.mapId;
+
+    db.query(query, [queryParams])
+      .then((point) => {
+        res.json(point.rows[0]);
+        // res.send(point.rows[0]);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
+  });
 
-    router.post("/:id/edit", (req, res) => {
-      let queryString = `
+  router.post("/:id/edit", (req, res) => {
+    let queryString = `
       UPDATE points
       SET title = $2, description = $3, image_url = $4
       WHERE id = $1
-      RETURNING *`
-      const queryParams = [req.params.id, req.body.title, req.body.description, req.body.imageURL];
-      
-      console.log("POST points/:id/edit req.params", req.params);
-      console.log("POST points/:id/edit req.body",req.body);
-      console.log("POST points/:id/edit queryString",queryString);
-      console.log("POST points/:id/edit queryParams",queryParams);
+      RETURNING *`;
+    const queryParams = [
+      req.params.id,
+      req.body.title,
+      req.body.description,
+      req.body.imageURL,
+    ];
 
-      db.query(queryString,queryParams)
-        .then(pointEdited => {
-          console.log("The point editted by the POST points/:id/edit is: ", pointEdited.rows[0]);
-          res.send(pointEdited.rows[0]);
-        })
-        .catch(err => {
-          console.log("YO our shit broke!!")
-          res
-            .status(500)
-            .json({ error: err.message });
-        });
-    });
+    console.log("POST points/:id/edit req.params", req.params);
+    console.log("POST points/:id/edit req.body", req.body);
+    console.log("POST points/:id/edit queryString", queryString);
+    console.log("POST points/:id/edit queryParams", queryParams);
 
-    router.post("/", (req, res) => {
-      let queryString = `
+    db.query(queryString, queryParams)
+      .then((pointEdited) => {
+        console.log(
+          "The point editted by the POST points/:id/edit is: ",
+          pointEdited.rows[0]
+        );
+        res.send(pointEdited.rows[0]);
+      })
+      .catch((err) => {
+        console.log("YO our shit broke!!");
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  router.post("/", (req, res) => {
+    let queryString = `
       INSERT INTO points (map_id, title, description, image_url, latitude, longitude)
       VALUES ($1, $2, $3, $4, $5, $6)
       RETURNING *;`;
-      const queryParams = [req.body.mapId, req.body.title, req.body.description, req.body.imageURL, req.body.latitude, req.body.longitude];
-      db.query(queryString,queryParams)
-        .then(pointAdded => {  
-          console.log("New point added in post route")
-          console.log("Returning from POST /points/: ", pointAdded.rows)
-          res.send(pointAdded.rows[0]);
-          //return pointAdded.rows[0];
-        })
-        .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message });
-        });
+    const queryParams = [
+      req.body.mapId,
+      req.body.title,
+      req.body.description,
+      req.body.imageURL,
+      req.body.latitude,
+      req.body.longitude,
+    ];
+    db.query(queryString, queryParams)
+      .then((pointAdded) => {
+        console.log("New point added in post route");
+        console.log("Returning from POST /points/: ", pointAdded.rows);
+        res.send(pointAdded.rows[0]);
+        //return pointAdded.rows[0];
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
-
+  });
 
   return router;
-}
-
+};
 
 module.exports = pointRouter;
